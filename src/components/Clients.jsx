cba2bdd1733479bd5fee1c80654119e1ffe6c71c
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-04-16 11:02:57 +0300

    For Clients and FourthProductCard made Desktop version.

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -3,11 +3,11 @@ import { brandIcons } from "../dummyData";
 export function Clients() {
   return (
     <>
-      <div className="flex flex-col mt-40 gap-10 pb-30">
+      <div className="flex flex-col lg:flex-row lg:mt-20 lg:items-center lg:gap-8  mt-40 gap-10 pb-30 lg:pb-10  lg:w-[100%] lg:justify-center lg:mx-auto">
         {brandIcons.map((item, index) => {
           return (
-            <div key={index} className="flex gap-4 mb-4">
-              <img src={item.src} alt="" />
+            <div key={index} className="flex gap-4 mb-4 lg:mb-0">
+              <img className="lg:w-[60%]" src={item.src} alt="" />
             </div>
           );
         })}
--- src/components/FourthProductCard.jsx
+++ src/components/FourthProductCard.jsx
@@ -3,17 +3,18 @@ import { bestSellerProducts2 } from "../dummyData";
 export function FourthProductCard() {
   return (
     <>
-      <div className="w-full flex justify-center mt-10 mb-10">
-        <h3 className="w-full h-8 !font-bold !text-2xl text-center">
+      <div className="w-full flex lg:flex-col lg:items-center justify-center mt-10 mb-10">
+        <h3 className="w-full h-8 !font-bold !text-2xl text-center lg:!text-start lg:w-[56.5%]">
           BESTSELLER PRODUCTS
         </h3>
+        <div className="lg:border-2 lg:border-[#ECECEC] lg:w-[56.5%] lg:mt-2"></div>
       </div>
-      <div>
+      <div className="lg:flex lg:gap-10">
         {bestSellerProducts2.map((item, index) => {
           return (
             <div key={index}>
-              <img className="mb-4" src={item.src} alt="" />
-              <div className="flex flex-col gap-2.5 ml-4">
+              <img className="mb-4 lg:w-[239px]" src={item.src} alt="" />
+              <div className="flex flex-col gap-2.5 ml-4 lg:items-start">
                 <h5 className="!font-bold !text-[16px] !text-[#252B42]">
                   {item.firstText}
                 </h5>
@@ -22,7 +23,7 @@ export function FourthProductCard() {
                 </h5>
               </div>
 
-              <div className="flex gap-2 ml-6 mt-2.5 mb-16">
+              <div className="flex gap-2 ml-6 mt-2.5 mb-16 lg:items-start lg:justify-start">
                 <h5 className="font-bold !text-[16px] !text-[#BDBDBD]">
                   {item.firstPrice}
                 </h5>
