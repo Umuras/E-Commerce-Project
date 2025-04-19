12784be3280630bedeb60d045b2f78e447a5a78a
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-04-19 11:31:56 +0300

    ProductDetailPage desktop approach finished.

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,6 +1,7 @@
 import { use, useEffect, useState } from "react";
 import {
   basketCircle,
+  bestSellerProducts2,
   cardItemPhoto,
   items,
   likeCircle,
@@ -30,8 +31,8 @@ export function ProductDetailPage({ setIsHomePage, isMobile }) {
   };
 
   return (
-    <section className="bg-[#FAFAFA] flex flex-col">
-      <div className="flex items-center justify-center gap-2 mt-4">
+    <section className="bg-[#FAFAFA] flex flex-col ">
+      <div className="flex items-center justify-center lg:justify-start lg:ml-32 gap-2 mt-4">
         <a
           className="font-bold text-center !text-[#252B42] !no-underline cursor-pointer"
           href="/home"
@@ -46,110 +47,101 @@ export function ProductDetailPage({ setIsHomePage, isMobile }) {
           Shop
         </a>
       </div>
-      {/* w-full: Bu div'in genişliğini ekranın tamamına yayar.
-      max-w-3xl: Ekranın belirli bir genişlik sınırına ulaşmasını sağlar (bu sınıfla 3xl boyutunu belirliyoruz).
-      mx-auto: Ekranda yatayda ortalamak için kullanılır.
-      overflow-hidden: Yalnızca bir slayt görünmesini sağlar, diğer slaytlar kaybolur.
-      rounded-lg: Kenarları yuvarlar.
-      shadow-lg: Kutunun etrafına gölge ekler. */}
-      <div className="relative lg:max-w-screen mx-auto overflow-hidden shadow-lg w-[348px] h-[277px] mt-24 mb-10">
-        {/* flex: Slaytları yatayda sıralar.
-      transition-transform duration-500: 500ms süresince geçiş animasyonu ekler.
-      style={{ transform: translateX(-${current * 100}%) }}: Bu satır, current state'ine bağlı olarak slaytları yatayda kaydırır. 
-      current değeri 0 ise, ilk slayt görünür.
-      current değeri 1 ise, ikinci slayt görünür. Yani her current değerinde slaytlar 100% kayar.*/}
-        <div
-          className="flex transition-transform duration-500  "
-          style={{ transform: `translateX(-${current * 100}%)` }}
-        >
-          {productDetailsItems.map((item, index) => (
-            //flex-shrink-0 için
-            //     Bu sınıf, bu div'in küçülmesini engeller (flexbox konteyner içindeyse).
-            //     Eğer parent’ı bir flex container ise, diğer elemanlar daralırken bu daralmaz.
-            //     Özellikle bir carousel veya yatay scroll alanında kullanılır.
-            // 📦 Örnek: Resim slider’ı düşün → tüm slide’ların genişliği sabit kalsın diye kullanılır.
-            //w-full ile parent ne kadar genişse, bu div de o kadar geniş olur.
-            <div key={index} className="relative flex-shrink-0 w-fit">
-              <picture>
-                <source srcSet={item.srcDesktop} media="(min-width: 1024px)" />
-                <img
-                  src={item.src}
-                  alt={item.alt}
-                  className="lg:w-screen lg:object-fill w-[348px] h-[277px]"
-                />
-              </picture>
-              {/* <img src={item.src} alt={item.alt} className="w-full h-full lg:w-screen lg:h-screen" /> */}
+
+      <section className="lg:flex gap-4">
+        <div className="lg:flex lg:flex-col">
+          <div className="relative lg:max-w-screen mx-auto lg:!ml-32 lg:w-[506px] lg:h-[405px] lg:!mx-0 overflow-hidden shadow-lg w-[348px] h-[277px] mt-24 lg:!mt-8 mb-10">
+            <div
+              className="flex transition-transform duration-500"
+              style={{ transform: `translateX(-${current * 100}%)` }}
+            >
+              {productDetailsItems.map((item, index) => (
+                <div key={index} className="relative flex-shrink-0 w-fit">
+                  <picture>
+                    <source
+                      srcSet={item.srcDesktop}
+                      media="(min-width: 1024px)"
+                    />
+                    <img
+                      src={item.src}
+                      alt={item.alt}
+                      className="lg:w-screen lg:object-full w-[348px] h-[277px] lg:h-[405px]"
+                    />
+                  </picture>
+                </div>
+              ))}
             </div>
-          ))}
+
+            {/* OK butonları */}
+            <button
+              onClick={prevSlide}
+              className="absolute top-1/2 left-2 transform -translate-y-1/2 text-white p-4 rounded-full"
+            >
+              <ChevronLeft />
+            </button>
+            <button
+              onClick={nextSlide}
+              className="absolute top-1/2 right-2 transform -translate-y-1/2  text-white p-4 rounded-full"
+            >
+              <ChevronRight />
+            </button>
+          </div>
+          <div className="flex gap-4 items-center justify-start ml-4 mb-10 lg:!ml-32">
+            {productDetailsItems.map((item, index) => {
+              return (
+                <section key={index}>
+                  <div
+                    className={`border-4  cursor-pointer ${
+                      current === index ? "shadow-xl shadow-amber-800" : ""
+                    }`}
+                    onClick={() => setCurrent(index)}
+                  >
+                    <img className="w-24 h-16" src={item.src} alt="" />
+                  </div>
+                </section>
+              );
+            })}
+          </div>
         </div>
 
-        {/* OK butonları */}
-        <button
-          onClick={prevSlide}
-          //top-1/2, parent elementin yüksekliğinin yarısı kadar yukarıdan konumlandırır.
-          //-translate-y-1/2 Elemanı kendi yüksekliğinin yarısı kadar yukarı kaydırır
-          className="absolute top-1/2 left-2 transform -translate-y-1/2 text-white p-4 rounded-full"
-        >
-          <ChevronLeft />
-        </button>
-        <button
-          onClick={nextSlide}
-          className="absolute top-1/2 right-2 transform -translate-y-1/2  text-white p-4 rounded-full"
-        >
-          <ChevronRight />
-        </button>
-      </div>
-      <div className="flex gap-4 items-center justify-start ml-4 mb-10">
-        {productDetailsItems.map((item, index) => {
-          return (
-            <section key={index}>
-              <div
-                className={`border-4  cursor-pointer ${
-                  current === index ? "shadow-xl shadow-amber-800" : ""
-                }`}
-                onClick={() => setCurrent(index)}
-              >
-                <img className="w-24 h-16" src={item.src} alt="" />
-              </div>
-            </section>
-          );
-        })}
-      </div>
-
-      <div className="flex flex-col items-start justify-start ml-8 w-[74%] mb-10">
-        <h4 className="!text-[20px] !text-[#252B42]">Hamburger</h4>
-        <div className="flex items-center justify-center gap-8 mt-2 mb-4">
-          <img src={stars} alt="" />
-          <h6 className="!text-[18px] !font-bold !text-[#737373] m-0">
-            10 Reviews
-          </h6>
+        <div className="flex flex-col items-start justify-start ml-8 w-[74%] mb-10 lg:!mt-14">
+          <h4 className="!text-[20px] !text-[#252B42]">Hamburger</h4>
+          <div className="flex items-center justify-center gap-8 mt-2 mb-4">
+            <img src={stars} alt="" />
+            <h6 className="!text-[18px] !font-bold !text-[#737373] m-0">
+              10 Reviews
+            </h6>
+          </div>
+          <h3 className="!text-[#252B42] !font-bold text-[24px]">$15</h3>
+          <div className="flex gap-2">
+            <h6 className="!text-[#737373] !text-[20px]">Availability : </h6>
+            <h6 className="!text-[#23A6F0] !text-[20px]">In Stock</h6>
+          </div>
+          <p className="!text-[14px] !text-[#858585] my-4 !font-bold w-[220px] text-left lg:w-[464px]">
+            Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
+            RELIT official consequent door ENIM RELIT Mollie. Excitation venial
+            consequent sent nostrum met.
+          </p>
+          <div className="border-1 w-full text-[#BDBDBD] lg:w-[464px]"></div>
+          <div className="flex gap-2 mt-10">
+            <button className="!rounded-[4px] !bg-[#23A6F0] !text-white py-2 px-3 !font-bold !text-[14px]">
+              Select Options
+            </button>
+            <img src={likeCircle} alt="" />
+            <img src={basketCircle} alt="" />
+            <img src={moreCircle} alt="" />
+          </div>
         </div>
-        <h3 className="!text-[#252B42] !font-bold text-[24px]">$15</h3>
-        <div className="flex gap-2">
-          <h6 className="!text-[#737373] !text-[20px]">Availability : </h6>
-          <h6 className="!text-[#23A6F0] !text-[20px]">In Stock</h6>
-        </div>
-        <p className="!text-[14px] !text-[#858585] my-4 !font-bold w-[220px] text-left">
-          Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
-          RELIT official consequent door ENIM RELIT Mollie. Excitation venial
-          consequent sent nostrum met.
-        </p>
-        <div className="border-1 w-full text-[#BDBDBD]"></div>
-        <div className="flex gap-2 mt-10">
-          <button className="!rounded-[4px] !bg-[#23A6F0] !text-white py-2 px-3 !font-bold !text-[14px]">
-            Select Options
-          </button>
-          <img src={likeCircle} alt="" />
-          <img src={basketCircle} alt="" />
-          <img src={moreCircle} alt="" />
-        </div>
-      </div>
+      </section>
 
       <section className="bg-white pt-4">
-        <section className="flex flex-col items-start justify-start ml-8 w-[88%] mb-10">
-          <nav className="flex gap-2">
+        <section className="flex flex-col items-start justify-start  lg:!ml-0  ml-8 w-[88%] mb-10 lg:w-full ">
+          <nav className="flex gap-2 lg:justify-center lg:w-full lg:!gap-20">
             <li className="list-none">
-              <a className="!text-[#737373] !text-[14px] !font-bold " href="#">
+              <a
+                className="!text-[#737373] !text-[14px] !font-bold lg:!no-underline "
+                href="#"
+              >
                 Description
               </a>
             </li>
@@ -170,30 +162,40 @@ export function ProductDetailPage({ setIsHomePage, isMobile }) {
               </a>
             </li>
           </nav>
-          <div className="flex flex-col items-start justify-start mt-4">
-            <img className="my-5" src={cardItemPhoto} alt="" />
-            <div className="flex flex-col">
-              <h3 className="!mb-10 !font-bold !text-[24px] !text-[#252B42]">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:gap-10 lg:justify-center lg:mx-50  lg:w-[80%] items-start justify-start mt-4">
+            <img
+              className="my-5 lg:!mt-18 lg:w-[400px]"
+              src={cardItemPhoto}
+              alt=""
+            />
+            <div className="flex flex-col lg:w-[20%] ">
+              <h3 className="!mb-10 !font-bold !text-[24px] !text-[#252B42] lg:!mt-14">
                 the quick fox jumps over
               </h3>
-              <h6 className="!text-[14px] !text-[#737373] !font-semibold mb-4 w-[90%]">
-                Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                sent. RELIT official consequent door ENIM RELIT Mollie.
-                Excitation venial consequent sent nostrum met. <br /> <br />
-                Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                sent. RELIT official consequent door ENIM RELIT Mollie.
-                Excitation venial consequent sent nostrum met. <br /> <br /> Met
-                minim Mollie non desert Alamo est sit cliquey dolor do met sent.
-                RELIT official consequent door ENIM RELIT Mollie. Excitation
-                venial consequent sent nostrum met.
-              </h6>
+              <div className="lg:flex lg:flex-col lg:w-[100%]">
+                <h6 className="!text-[14px] !text-[#737373] !font-semibold mb-4 w-[90%]">
+                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                  sent. RELIT official consequent door ENIM RELIT Mollie.
+                  Excitation venial consequent sent nostrum met.
+                </h6>
+                <h6 className="!text-[14px] !text-[#737373] !font-semibold mb-4 w-[90%]">
+                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                  sent. RELIT official consequent door ENIM RELIT Mollie.
+                  Excitation venial consequent sent nostrum met.
+                </h6>
+                <h6 className="!text-[14px] !text-[#737373] !font-semibold mb-4 w-[90%]">
+                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                  sent. RELIT official consequent door ENIM RELIT Mollie.
+                  Excitation venial consequent sent nostrum met.
+                </h6>
+              </div>
             </div>
 
             <div className="flex flex-col">
               {Array.from({ length: 2 }).map((_, index) => {
                 return (
                   <section key={index}>
-                    <h3 className="!mb-6 !font-bold !text-[24px] !text-[#252B42] !mt-4">
+                    <h3 className="!mb-6 !font-bold !text-[24px] !text-[#252B42] !mt-4 ">
                       the quick fox jumps over
                     </h3>
                     <div className="flex flex-col gap-3 items-center justify-center ">
@@ -227,6 +229,32 @@ export function ProductDetailPage({ setIsHomePage, isMobile }) {
       <section>
         <div className="flex flex-col items-center justify-center">
           <FourthProductCard />
+          <div className="lg:flex lg:gap-10">
+            {bestSellerProducts2.map((item, index) => {
+              return (
+                <div key={index}>
+                  <img className="mb-4 lg:w-[239px]" src={item.src} alt="" />
+                  <div className="flex flex-col gap-2.5 ml-4 lg:items-start">
+                    <h5 className="!font-bold !text-[16px] !text-[#252B42]">
+                      {item.firstText}
+                    </h5>
+                    <h5 className="!font-bold !text-[14px] !text-[#737373]">
+                      {item.secondText}
+                    </h5>
+                  </div>
+
+                  <div className="flex gap-2 ml-6 mt-2.5 mb-16 lg:items-start lg:justify-start">
+                    <h5 className="font-bold !text-[16px] !text-[#BDBDBD]">
+                      {item.firstPrice}
+                    </h5>
+                    <h5 className="font-bold !text-[16px] !text-[#23856D]">
+                      {item.secondPrice}
+                    </h5>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
           <Clients />
         </div>
       </section>
